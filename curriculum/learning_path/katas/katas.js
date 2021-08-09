c96b023a112ab4ed1c70c770f1b6baa8856403ba
Borbély Tamás <borblytams@gmail.com>
2021-08-09 09:45:00 +0200

    adding katas.js to hold solved katas

---
+++ curriculum/learning_path/katas/katas.js
@@ -0,0 +1,153 @@
+export const katas = [
+    {katas: [
+                            {
+                                id: 1, 
+                                kata_name: `even_or_odd`, 
+                                date: `2021.08.03`, 
+                                done: false, 
+                                difficulty: ``, 
+                                questions: [], 
+                                ideas: [], 
+                                problems: []
+                            },
+                            {
+                                id: 2, 
+                                kata_name: `sum_of_opposite`,
+                                date: `2021.08.03`, 
+                                done: false, 
+                                difficulty: ``, 
+                                questions: [], 
+                                ideas: [], 
+                                problems: []
+                            },
+                            {
+                                id: 3, 
+                                kata_name: `opposite_number`,
+                                date: `2021.08.03`,  
+                                done: false, 
+                                difficulty: ``, 
+                                questions: [], 
+                                ideas: [], 
+                                problems: []
+                            },
+                            {
+                                id: 4, 
+                                kata_name: `remove_first_and_last_character`,
+                                date: `2021.08.03`,  
+                                done: false, 
+                                difficulty: ``, 
+                                questions: [], 
+                                ideas: [], 
+                                problems: []
+                            },
+                            {
+                                id: 5, 
+                                kata_name: `string_repeat`,
+                                date: `2021.08.03`,  
+                                done: false, 
+                                difficulty: ``, 
+                                questions: [], 
+                                ideas: [], 
+                                problems: []
+                            },
+                            {
+                                id: 6, 
+                                kata_name: `remove_string_spaces`,
+                                date: `2021.08.03`,  
+                                done: false, 
+                                difficulty: ``,
+                                questions: [], 
+                                ideas: [], 
+                                problems: []
+                            },
+                            {
+                                id: 7, 
+                                kata_name: `find_the_smallest_integer_in_array`,
+                                date: `2021.08.03`, 
+                                done: false, 
+                                difficulty: ``, 
+                                questions: [], 
+                                ideas: [], 
+                                problems: []
+                            },
+                            {
+                                id: 8, 
+                                kata_name: `convert_boolean_values_to_strings_yes_or_no`,
+                                date: `2021.08.03`,  
+                                done: false, 
+                                difficulty: ``, 
+                                questions: [], 
+                                ideas: [], 
+                                problems: []
+                            },
+                            {
+                                id: 9, 
+                                kata_name: `a_wolf_in_sheeps_clothing`,
+                                date: `2021.08.03`,  
+                                done: false, 
+                                difficulty: ``, 
+                                questions: [], 
+                                ideas: [], 
+                                problems: []
+                            },
+                            {
+                                id: 10, 
+                                kata_name: `convert_a_number_to_a_string`,
+                                date: `2021.08.03`,  
+                                done: false, 
+                                difficulty: ``, 
+                                questions: [], 
+                                ideas: [], 
+                                problems: []
+                            },
+                            {
+                                id: 11, 
+                                kata_name: `basic_mathetmatical_operations`,
+                                date: `2021.08.03`,  
+                                done: false, 
+                                difficulty: ``, 
+                                questions: [], 
+                                ideas: [], 
+                                problems: []
+                            },
+                            {
+                                id: 12, 
+                                kata_name: `convert_number_to_reversed_array_of_digits`,
+                                date: `2021.08.03`,  
+                                done: false, 
+                                difficulty: ``, 
+                                questions: [], 
+                                ideas: [], 
+                                problems: []
+                            },
+                            {
+                                id: 13, 
+                                kata_name: `abbreviate_a_two_word_name`,
+                                date: `2021.08.03`,  
+                                done: false, 
+                                difficulty: ``, 
+                                questions: [], 
+                                ideas: [], 
+                                problems: []
+                            },
+                            {
+                                id: 14, 
+                                kata_name: `if_you_cant_sleep_just_count_sheep`,
+                                date: `2021.08.03`,  
+                                done: false, 
+                                difficulty: ``, 
+                                questions: [], 
+                                ideas: [], 
+                                problems: []
+                            },
+                            {
+                                id: 15, 
+                                kata_name: `convert_a_boolean_to_a_string`,
+                                date: `2021.08.03`,  
+                                done: false, 
+                                difficulty: ``, 
+                                questions: [], 
+                                ideas: [], 
+                                problems: []
+                            }
+]
